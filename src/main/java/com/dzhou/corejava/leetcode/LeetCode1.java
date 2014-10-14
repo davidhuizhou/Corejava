f589d526c6dc06c80580509db4657f1972d4c441
Hui Zhou <davidhuizhou@gmail.com>
2014-10-13 21:00:58 -0400

    no message

--- src/main/java/com/dzhou/corejava/leetcode/LeetCode1.java
+++ src/main/java/com/dzhou/corejava/leetcode/LeetCode1.java
@@ -752,6 +752,37 @@ public class LeetCode1 {
 
     }
 
+    public static void matrixChainOrder(int[] p, int[][] m, int[][] s) {
+        int n = p.length - 1;
+        for (int i = 1; i <= n; i++)
+            m[i][i] = 0;
+
+        for (int l = 2; l <= n; l++) {     //This is needed since each l matrix multiplication is different at different i
+            for (int i = 1; i <= n - l + 1; i++) {      ////This is needed since each l matrix multiplication is different at different i
+                int j = i + l - 1;
+                m[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    if (q < m[i][j]) {
+                        m[i][j] = q;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printOptimalParense(int[][] s, int i, int j){
+        if(i == j)
+            System.out.print("A" + i);
+        else {
+            System.out.print("(");
+            printOptimalParense(s, i, s[i][j]);
+            printOptimalParense(s, s[i][j] + 1, j);
+            System.out.print(")");
+        }
+    }
+
     public static void main(String[] args){
 
         System.out.println("Test maxProfit3");
@@ -884,6 +915,17 @@ public class LeetCode1 {
         for(int i = 1; i <=10; i++){
             printCutRodSolution(p, i);
         }
+
+
+        int[] dimension = {30, 35, 15, 5, 10, 20, 25};
+        int[][] mm = new int[7][7];
+        int[][] ss = new int[7][7];
+
+        matrixChainOrder(dimension, mm, ss);
+        printOptimalParense(ss, 1, 6);
+
+
+
 
 
     }
