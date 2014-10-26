e085a47e8b932ecb5a52ca7122e483a75688f0ef
Hui Zhou <davidhuizhou@gmail.com>
2014-10-25 22:03:58 -0400

    no message

--- src/main/java/com/dzhou/corejava/leetcode/LeetCode2.java
+++ src/main/java/com/dzhou/corejava/leetcode/LeetCode2.java
@@ -256,8 +256,223 @@ public class LeetCode2 {
     }
 
 
+    /**
+     * https://oj.leetcode.com/problems/linked-list-cycle/
+     * http://www.programcreek.com/2012/12/leetcode-linked-list-cycle/
+     * https://oj.leetcode.com/submissions/detail/13801206/
+     *
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode p = head;
+        ListNode q = head;
+
+        while (p != null && q != null) {
+            p = p.next;
+            q = q.next;
+            if (q != null)
+                q = q.next;
+
+            if (p != null && p == q) // has to check for null
+                return true;
+        }
+        return false;
 
 
+    }
+
+    /**
+     * https://oj.leetcode.com/problems/linked-list-cycle-ii/
+     * https://oj.leetcode.com/submissions/detail/13802485/
+     */
+    public ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return null;
+        boolean hasCycle = false;
+        ListNode p = head;
+        ListNode q = head;
+        while (p != null && q != null) {
+            p = p.next;
+            q = q.next;
+            if (q != null)
+                q = q.next;
+
+            if (p != null && p == q) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if (hasCycle) {
+            p = head;
+            while (p != q) {
+                p = p.next;
+                q = q.next;
+            }
+            return p;
+        }
+
+        return null;
+    }
+
+    /**
+     * https://oj.leetcode.com/problems/reorder-list/
+     * http://www.programcreek.com/2013/12/in-place-reorder-a-singly-linked-list-in-java/
+     * https://oj.leetcode.com/submissions/detail/13805311/
+     */
+    public static void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null)
+            return;
+
+        int count = countList(head);
+
+        if (count <= 2)
+            return;
+
+        ListNode p = head;
+        for (int i = 0; i < (count + 1) / 2 - 1; i++) {
+            p = p.next;
+        }
+        ListNode q = p.next;
+        p.next = null;
+
+        q = reverseList(q);
+        mergeList(head, q);
+
+
+    }
+
+
+
+
+    private static int countList(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+
+    private static ListNode reverseList(ListNode head) {
+        if (head == null)
+            return head;
+
+        ListNode r = head;
+        ListNode p = head.next;
+        r.next = null;
+
+        while (p != null) {
+            ListNode q = p.next;
+            p.next = r;
+            r = p;
+            p = q;
+        }
+        return r;
+    }
+
+    private static void mergeList(ListNode head1, ListNode head2) {
+        ListNode p = head1;
+        ListNode q = head2;
+        while (p != null && q != null) {
+            ListNode m = p.next;
+            ListNode n = q.next;
+            p.next = q;
+            q.next = m;
+            p = m;
+            q = n;
+        }
+
+    }
+
+    /**
+     * https://oj.leetcode.com/problems/binary-tree-preorder-traversal/
+     * https://oj.leetcode.com/submissions/detail/13806713/
+     * https://oj.leetcode.com/submissions/detail/13807003/
+     * @return
+     */
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null)
+            return result;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left != null)
+                stack.push(node.left);
+        }
+        return result;
+
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        preorderTraversal(root, list);
+        return list;
+    }
+
+    private void preorderTraversal(TreeNode root, List<Integer> list){
+        if(root == null)
+            return;
+        list.add(root.val);
+        preorderTraversal(root.left, list);
+        preorderTraversal(root.right, list);
+
+    }
+
+    /**
+     * https://oj.leetcode.com/problems/binary-tree-postorder-traversal/
+     * https://oj.leetcode.com/submissions/detail/13807410/
+     * https://oj.leetcode.com/submissions/detail/13807508/
+     */
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null)
+            return result;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left == null && node.right == null)
+                result.add(node.val);
+            else {
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                TreeNode r = new TreeNode(node.val);
+                stack.push(r);
+                if (right != null)
+                    stack.push(right);
+                if (left != null)
+                    stack.push(left);
+            }
+        }
+        return result;
+
+    }
+
+    public List<Integer> postrderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        postrderTraversal(root, list);
+        return list;
+    }
+
+    private void postrderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+
+        postrderTraversal(root.left, list);
+        postrderTraversal(root.right, list);
+        list.add(root.val);
+
+    }
 
     public static void main(String[] args){
       int[] ratings = new int[12000];
@@ -302,6 +517,33 @@ public class LeetCode2 {
 
         for(String ss : list)
             System.out.println(ss);
+
+
+        int[] nodes = {1,2,3,4,5};
+        ListNode head = new ListNode(nodes[0]);
+        ListNode p = head;
+
+        for(int i = 1; i < nodes.length; i++){
+            p.next = new ListNode(nodes[i]);
+            p = p.next;
+        }
+
+        reorderList(head);
+        p = head;
+
+        while(p != null){
+            System.out.print(p.val + " ");
+            p = p.next;
+        }
+
+        System.out.println();
+        LRUCache lru = new LRUCache(2);
+        lru.set(2, 1);
+        lru.set(1, 1);
+        System.out.println(lru.get(2));
+        lru.set(4, 1);
+        System.out.println(lru.get(1));
+        System.out.println(lru.get(2));
 
 
     }
