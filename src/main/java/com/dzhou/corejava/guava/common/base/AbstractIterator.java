d6d713e36d32fb82b433c15aa829f2b7498aed2d
Hui Zhou <davidhuizhou@gmail.com>
2016-07-04 15:04:09 -0400

    Splitter.

--- src/main/java/com/dzhou/corejava/guava/common/base/AbstractIterator.java
+++ src/main/java/com/dzhou/corejava/guava/common/base/AbstractIterator.java
@@ -9,60 +9,60 @@ import static com.google.common.base.Preconditions.checkState;
  * Created by huizhou on 12/11/15.
  */
 abstract class AbstractIterator<T> implements Iterator<T> {
-    private State state = State.NOT_READY;
+  private State state = State.NOT_READY;
 
-    protected AbstractIterator() {
-    }
+  protected AbstractIterator() {
+  }
 
-    private enum State {
-        READY,
-        NOT_READY,
-        DONE,
-        FAILED,
-    }
+  private enum State {
+    READY,
+    NOT_READY,
+    DONE,
+    FAILED,
+  }
 
-    private T next;
+  private T next;
 
-    protected abstract T computeNext();
+  protected abstract T computeNext();
 
-    protected final T endOfData() {
-        state = State.DONE;
-        return null;
-    }
+  protected final T endOfData() {
+    state = State.DONE;
+    return null;
+  }
 
-    public final boolean hasNext() {
-        checkState(state != State.FAILED);
-        switch (state) {
-            case READY:
-                return true;
-            case DONE:
-                return false;
-            default:
-        }
-        return tryComputeNext();
-    }
-
-    private boolean tryComputeNext() {
-        state = State.FAILED;
-        next = computeNext();
-        if (state != State.DONE) {
-            state = State.READY;
-            return true;
-        }
+  public final boolean hasNext() {
+    checkState(state != State.FAILED);
+    switch (state) {
+      case READY:
+        return true;
+      case DONE:
         return false;
+      default:
     }
+    return tryComputeNext();
+  }
 
-    public final T next() {
-        if (!hasNext()) {
-            throw new NoSuchElementException();
-        }
-        state = State.NOT_READY;
-        T result = next;
-        next = null;
-        return result;
+  private boolean tryComputeNext() {
+    state = State.FAILED;
+    next = computeNext();
+    if (state != State.DONE) {
+      state = State.READY;
+      return true;
     }
+    return false;
+  }
 
-    public final void remove() {
-        throw new UnsupportedOperationException();
+  public final T next() {
+    if (!hasNext()) {
+      throw new NoSuchElementException();
     }
+    state = State.NOT_READY;
+    T result = next;
+    next = null;
+    return result;
+  }
+
+  public final void remove() {
+    throw new UnsupportedOperationException();
+  }
 }
--- src/main/java/com/dzhou/corejava/guava/common/base/Splitter.java
+++ src/main/java/com/dzhou/corejava/guava/common/base/Splitter.java
@@ -5,150 +5,138 @@ import com.google.common.base.CharMatcher;
 
 import java.util.Iterator;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 /**
  * Created by huizhou on 12/11/15.
  */
 public final class Splitter {
-    private final CharMatcher trimmer;
-    private final boolean omitEmptyStrings;
-    private final int limit;
-    private final Strategy strategy;
+  private final CharMatcher trimmer;
+  private final boolean omitEmptyStrings;
+  private final Strategy strategy;
+  private final int limit;
 
-    private Splitter(Strategy strategy) {
-        this(strategy, CharMatcher.none(), false, Integer.MAX_VALUE);
+  private Splitter(Strategy strategy) {
+    this(strategy, false, CharMatcher.none(), Integer.MAX_VALUE);
+  }
+
+  private Splitter(Strategy strategy, boolean omitEmptyStrings, CharMatcher trimmer, int limit) {
+    this.strategy = strategy;
+    this.trimmer = trimmer;
+    this.omitEmptyStrings = omitEmptyStrings;
+    this.limit = limit;
+  }
+
+  private interface Strategy {
+    Iterator<String> iterator(Splitter splitter, CharSequence toSplit);
+  }
+
+  public static Splitter on(char separator) {
+    return on(CharMatcher.is(separator));
+  }
+
+  public static Splitter on(final CharMatcher separatorMatcher) {
+    return new Splitter(
+        new Strategy() {
+          @Override
+          public SplittingIterator iterator(Splitter splitter, final CharSequence toSplit) {
+            return new SplittingIterator(splitter, toSplit) {
+              @Override
+              int separatorStart(int start) {
+                return separatorMatcher.indexIn(toSplit, start);
+              }
+
+              @Override
+              int separatorEnd(int separatorPosition) {
+                return separatorPosition + 1;
+              }
+            };
+          }
+        });
+  }
+
+  public Iterable<String> split(final CharSequence sequence) {
+    return new Iterable<String>() {
+      @Override
+      public Iterator<String> iterator() {
+        return splittingIterator(sequence);
+      }
+
+      @Override
+      public String toString() {
+        return Joiner.on(",")
+            .appendTo(new StringBuilder("["), this)
+            .append("]")
+            .toString();
+      }
+    };
+  }
+
+  private Iterator<String> splittingIterator(final CharSequence sequence) {
+    return strategy.iterator(this, sequence);
+  }
+
+  private abstract static class SplittingIterator extends AbstractIterator<String> {
+    final CharSequence toSplit;
+    final CharMatcher trimmer;
+    final boolean omitEmptyStrings;
+
+    abstract int separatorStart(int start);
+
+    abstract int separatorEnd(int separatorPosition);
+
+    int offset = 0;
+    int limit;
+
+    protected SplittingIterator(Splitter splitter, CharSequence toSplit) {
+      this.toSplit = toSplit;
+      this.trimmer = splitter.trimmer;
+      this.omitEmptyStrings = splitter.omitEmptyStrings;
+      this.limit = splitter.limit;
     }
 
-    private Splitter(Strategy strategy, CharMatcher trimmer, boolean omitEmptyStrings, int limit) {
-        this.strategy = strategy;
-        this.trimmer = trimmer;
-        this.omitEmptyStrings = omitEmptyStrings;
-        this.limit = limit;
-    }
-
-    public static Splitter on(char c) {
-        return on(CharMatcher.is(c));
-    }
-
-    public static Splitter on(CharMatcher separatorMatcher) {
-        checkNotNull(separatorMatcher);
-        return new Splitter(
-                new Strategy() {
-                    public SplittingIterator iterator(Splitter splitter, CharSequence
-                            toSplit) {
-                        return new SplittingIterator(splitter, toSplit) {
-                            @Override
-                            int separatorStart(int start) {
-                                return separatorMatcher.indexIn(toSplit, start);
-                            }
-
-                            @Override
-                            int separatorEnd(int separatorPosition) {
-                                return separatorPosition + 1;
-                            }
-                        };
-                    }
-                }
-        );
-    }
-
-    public Iterable<String> split(final CharSequence sequence) {
-        checkNotNull(sequence);
-
-        return new Iterable<String>() {
-            @Override
-            public Iterator<String> iterator() {
-                return splittingIterator(sequence);
-            }
-
-            @Override
-            public String toString() {
-                return Joiner.on(",")
-                        .appendTo(new StringBuilder().append('['), this)
-                        .append(']')
-                        .toString();
-            }
-        };
-    }
-
-    private Iterator<String> splittingIterator(CharSequence sequence) {
-        return strategy.iterator(this, sequence);
-    }
-
-
-    private interface Strategy {
-        Iterator<String> iterator(Splitter splitter, CharSequence toSplit);
-    }
-
-    private abstract static class SplittingIterator extends AbstractIterator<String> {
-        final CharMatcher trimmer;
-        final boolean omitEmptyStrings;
-        final CharSequence toSplit;
-
-        abstract int separatorStart(int start);
-
-        abstract int separatorEnd(int separatorPosition);
-
-        int offset = 0;
-        int limit;
-
-        protected SplittingIterator(Splitter splitter, CharSequence toSplit) {
-            this.trimmer = splitter.trimmer;
-            this.omitEmptyStrings = splitter.omitEmptyStrings;
-            this.limit = splitter.limit;
-            this.toSplit = toSplit;
+    @Override
+    protected String computeNext() {
+      int nextStart = offset;
+      while (offset != -1) {
+        int start = nextStart;
+        int end;
+        int separatorPosition = separatorStart(offset);
+        if (separatorPosition == -1) {
+          end = toSplit.length();
+          offset = -1;
+        } else {
+          end = separatorPosition;
+          offset = separatorEnd(separatorPosition);
         }
-
-        @Override
-        protected String computeNext() {
-            int nextStart = offset;
-            while (offset != -1) {
-                int start = nextStart;
-                int end;
-                int separatorPosition = separatorStart(offset);
-                if (separatorPosition == -1) {
-                    end = toSplit.length();
-                    offset = -1;
-                } else {
-                    end = separatorPosition;
-                    offset = separatorEnd(separatorPosition);
-                }
-
-                if (offset == nextStart) {
-                    offset++;
-                    if (offset >= toSplit.length()) {
-                        offset = -1;
-                    }
-                    continue;
-                }
-
-                while (start < end && trimmer.matches(toSplit.charAt(start))) {
-                    start++;
-                }
-
-                while (end > start && trimmer.matches(toSplit.charAt(end - 1))) {
-                    end--;
-                }
-
-                if (omitEmptyStrings && start == end) {
-                    nextStart = offset;
-                    continue;
-                }
-
-                if (limit == 1) {
-                    end = toSplit.length();
-                    offset = -1;
-                    while (end > start && trimmer.matches(toSplit.charAt(end))) {
-                        end--;
-                    }
-                } else {
-                    limit--;
-                }
-                return toSplit.subSequence(start, end).toString();
-            }
-            return endOfData();
+        if (offset == nextStart) {
+          offset++;
+          if (offset >= toSplit.length()) {
+            offset = -1;
+          }
+          continue;
         }
+        while (start < end && trimmer.matches(toSplit.charAt(start))) {
+          start++;
+        }
+        while (end > start && trimmer.matches(toSplit.charAt(end - 1))) {
+          end--;
+        }
+        if (omitEmptyStrings && start == end) {
+          nextStart = offset;
+          continue;
+        }
+        if (limit == 1) {
+          end = toSplit.length();
+          offset = -1;
+          while (end > start && trimmer.matches(toSplit.charAt(end - 1))) {
+            end--;
+          }
+        } else {
+          limit--;
+        }
+        return toSplit.subSequence(start, end).toString();
+      }
+      return endOfData();
     }
 
+  }
 }
