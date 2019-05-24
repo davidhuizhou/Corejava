395ce51ba19b5c1e5bf510661fae9a62299caf84
Hui Zhou <davidhuizhou@gmail.com>
2019-05-24 11:03:53 -0400

    orderOfClosingResources

--- src/test/java/com/dzhou/corejava/java_programming_language_4/TryWithResources.java
+++ src/test/java/com/dzhou/corejava/java_programming_language_4/TryWithResources.java
@@ -1,7 +1,23 @@
 package com.dzhou.corejava.java_programming_language_4;
 
 public class TryWithResources {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+
+    orderOfClosingResources();
+
+    supressExceptions();
+  }
+
+  private static void orderOfClosingResources() throws Exception {
+    try (AutoCloseableResourcesFirst af = new AutoCloseableResourcesFirst();
+         AutoCloseableResourcesSecond as = new AutoCloseableResourcesSecond()) {
+
+      af.doSomething();
+      as.doSomething();
+    }
+  }
+
+  private static void supressExceptions() {
     try (One one = new One(); Two two = new Two()) {
       System.out.println("try");
       throw new Exception("try exception");
@@ -16,7 +32,6 @@ public class TryWithResources {
     }
   }
 
-
   private static class One implements AutoCloseable {
     public void close() throws Exception {
       System.out.println("Close - One");
@@ -30,5 +45,38 @@ public class TryWithResources {
       throw new Exception("Close - Two exception");
     }
   }
+
+  public static class AutoCloseableResourcesFirst implements AutoCloseable {
+
+    public AutoCloseableResourcesFirst() {
+      System.out.println("Constructor -> AutoCloseableResources_First");
+    }
+
+    public void doSomething() {
+      System.out.println("Something -> AutoCloseableResources_First");
+    }
+
+    @Override
+    public void close() throws Exception {
+      System.out.println("Closed AutoCloseableResources_First");
+    }
+  }
+
+  public static class AutoCloseableResourcesSecond implements AutoCloseable {
+
+    public AutoCloseableResourcesSecond() {
+      System.out.println("Constructor -> AutoCloseableResources_Second");
+    }
+
+    public void doSomething() {
+      System.out.println("Something -> AutoCloseableResources_Second");
+    }
+
+    @Override
+    public void close() throws Exception {
+      System.out.println("Closed AutoCloseableResources_Second");
+    }
+  }
+
 
 }
